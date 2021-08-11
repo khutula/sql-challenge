6b82266bb0a6e654842ee0b8bd21fa7002342549
Karina Hutula <khutula@g.clemson.edu>
2021-08-11 15:24:00 -0400

    add code for query 7 and 8

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -31,3 +31,16 @@ FROM Department_Employees AS de
 JOIN Departments AS d USING (dept_no)
 JOIN Employees AS e USING (emp_no)
 WHERE d.dept_name = 'Sales';
+
+-- Sales & Development Departments: employee number, last name, first name, dept name
+SELECT de.emp_no, e.last_name, e.first_name, d.dept_name
+FROM Department_Employees AS de
+JOIN Departments AS d USING (dept_no)
+JOIN Employees AS e USING (emp_no)
+WHERE d.dept_name = 'Sales' OR d.dept_name = 'Development';
+
+-- Last name frequency
+SELECT last_name, COUNT(last_name) AS last_name_count
+FROM Employees
+GROUP BY last_name
+ORDER BY last_name_count DESC
