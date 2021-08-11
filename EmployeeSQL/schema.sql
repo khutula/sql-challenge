621792fdd370a2f1be73fbecdf9185b03ff8efab
Karina Hutula <khutula@g.clemson.edu>
2021-08-11 14:15:09 -0400

    create table schema sql

---
+++ EmployeeSQL/schema.sql
@@ -0,0 +1,42 @@
+CREATE TABLE Titles (
+	title_id VARCHAR(5) PRIMARY KEY,
+	title VARCHAR NOT NULL
+);
+
+CREATE TABLE Employees (
+	emp_no INT NOT NULL PRIMARY KEY,
+	emp_title VARCHAR(5) NOT NULL,
+	FOREIGN KEY (emp_title) REFERENCES Titles(title_id),
+	birth_date VARCHAR NOT NULL,
+	first_name VARCHAR NOT NULL,
+	last_name VARCHAR NOT NULL,
+	sex VARCHAR(1) NOT NULL,
+	hire_date VARCHAR NOT NULL
+);
+
+CREATE TABLE Salaries (
+	emp_no INT NOT NULL PRIMARY KEY,
+	FOREIGN KEY (emp_no) REFERENCES Employees(emp_no),
+	salary INT NOT NULL
+);
+
+CREATE TABLE Departments (
+	dept_no VARCHAR(4) NOT NULL PRIMARY KEY,
+	dept_name VARCHAR NOT NULL
+);
+
+CREATE TABLE Department_Employees (
+	emp_no INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES Employees(emp_no),
+	dept_no VARCHAR(4) NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES Departments(dept_no),
+	PRIMARY KEY (emp_no, dept_no)
+);
+
+CREATE TABLE Department_Managers (
+	emp_no INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES Employees(emp_no),
+	dept_no VARCHAR(4) NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES Departments(dept_no),
+	PRIMARY KEY (emp_no, dept_no)
+);
