e26cac00977a3d50d69a5c62bbb4851adf25d7d1
Karina Hutula <khutula@g.clemson.edu>
2021-08-11 15:16:04 -0400

    add code for query 5 and 6

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -19,3 +19,15 @@ SELECT de.emp_no, e.last_name, e.first_name, d.dept_name
 FROM Department_Employees AS de
 JOIN Departments AS d USING (dept_no)
 JOIN Employees AS e USING (emp_no);
+
+-- Hercules B: first name, last name, sex
+SELECT first_name, last_name, sex
+FROM Employees
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
+
+-- Sales Department: employee number, last name, first name, dept name
+SELECT de.emp_no, e.last_name, e.first_name, d.dept_name
+FROM Department_Employees AS de
+JOIN Departments AS d USING (dept_no)
+JOIN Employees AS e USING (emp_no)
+WHERE d.dept_name = 'Sales';
